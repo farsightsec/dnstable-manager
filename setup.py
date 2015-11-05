83be037d81e9af66324684edeaae7181c2b1a9c7
Henry Stern <stern@fsi.io>
2015-11-05 08:00:32 -0400

    Version 0.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'dnstable-manager',
-    version = '0.5',
+    version = '0.6',
     packages = find_packages(),
 
     scripts = ['dnstable-manager'],
